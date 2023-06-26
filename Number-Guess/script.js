43850844a06b66b4284b0935da5016cfc77636cc
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-06-26 20:49:23 +1000

    finished number guessing

--- Number-Guess/script.js
+++ Number-Guess/script.js
@@ -1,52 +1,61 @@
 var playerGuess = 0;
 var playerScore = 0;
 var computerScore = 0;
-var randomNumber = 0;
+var randomNumber = Math.floor(Math.random() * 10) + 1;
 var playerTip = "Guess a number between 1 and 10";
-var countDownFrom = 10;
-var currentSecond = 11;
+var currentSecond = 10;
 var timerId = 0;
+var restartGame = 1;
 
 function gameLoop() {
     equalityCheck();
-    if (currentSecond = 0) {
-        computerScore = Math.floor(Math.random() * 10) + 1;
+    if (restartGame == 1) {
+        restartGame = 0;
+        randomNumber = Math.floor(Math.random() * 10) + 1;
+        currentSecond = 11;
         secondCountDown();
     }
     updateHtmlContent();
 }
 
 function equalityCheck() {
-    computerScore = randomNumber;
     playerGuess = Number(document.getElementById("playerGuessInput").value);
     if (isNaN(playerGuess)) {
         playerTip = "Not a valid answer";
     } else if (playerGuess == randomNumber) {
-          currentSecond = 11;
-          playerScore++;
-          playerTip = "Correct, click to play again";
-          document.getElementById("playButton").value = "Play now";
-      } else if (playerGuess > randomNumber) {
+        playerScore++;
+        playerTip = "Correct, click to play again";
+        clearTimeout(timerId);
+        document.getElementById("playButton").value = "Play now";
+     } else if (playerGuess > randomNumber) {
           playerTip = "Your guess is too high";
-          document.getElementById("playButton").value = "Guess again";
-      } else {
+        document.getElementById("playButton").value = "Guess again";
+        computerScore++;
+     } else {
           playerTip = "Your guess is too low";
-          document.getElementById("playButton").value = "Guess again";
+        document.getElementById("playButton").value = "Guess again";
+        computerScore++;
       }
 }
 
 function updateHtmlContent() {
+    if (playerTip == "Correct, click to play again") {
+        restartGame = 1;
+    }
     document.getElementById("playerScoreContent").innerHTML = playerScore;
     document.getElementById("computerScoreContent").innerHTML = computerScore;
     document.getElementById("tipContent").innerHTML = playerTip;
-    document.getElementById("countDown").innerHTML = currentSecond;
 }
 
 function secondCountDown() {
     if (currentSecond != 0) {
         currentSecond--;
-
     } else {
-
+        clearTimeout(timerId);
+        playerTip = "Game over, you lose!";
+        restartGame = 1;
+        return;
     }
+    document.getElementById("countDown").innerHTML = currentSecond;
+    timerId = setTimeout(secondCountDown, 1000);
 }
