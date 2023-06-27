d915f0716ca88fc6050800cd3d5d90aed4b2a6ee
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-06-28 09:53:28 +1000

    coulor

--- Text Puzzle/script.js
+++ Text Puzzle/script.js
@@ -9,3 +9,25 @@
 
 var x = document.getElementById("playerWin");
 x.style.display = "block";
+var redValue = 255;
+var blueValue = 0;
+var greenValue = 0;
+var timerId = 0;
+changeCoulor();
+function changeCoulor() {
+    x.style.color = "rgb(" + redValue + ", " + blueValue + ", " + greenValue + ")";
+    if (redValue == 255 && greenValue != 255 && blueValue == 0) {
+        greenValue++;
+    } else if (greenValue == 255 && redValue != 0 && blueValue == 0) {
+        redValue--;
+    } else if (redValue == 0 && greenValue == 255 && blueValue != 255) {
+        blueValue++;
+    } else if (blueValue == 255 && greenValue != 0) {
+        greenValue--;
+    } else if (blueValue == 255 && redValue != 255) {
+        redValue++;
+    } else if (redValue == 255) {
+        blueValue--;
+    }
+    timerId = setTimeout("changeCoulor()", 1);
+}
